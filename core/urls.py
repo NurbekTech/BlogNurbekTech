17e678b84dfa9de29394068758517674c8b5224c
nurbekomar <omarnurbek13@gmail.com>
2024-12-11 16:04:28 +0500

    handler404

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from blog.views import pageNotFound
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls'))
 ]
+
+handler404 = pageNotFound
