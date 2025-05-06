d3944c8ebd057483b5c9eb7149411dac3351bebc
nurbekomar <omarnurbek13@gmail.com>
2025-05-06 17:06:04 +0500

    update theme admin

--- core/settings.py
+++ core/settings.py
@@ -127,3 +127,7 @@ MEDIA_ROOT = os.path.join("media/")
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+JAZZMIN_SETTINGS = {
+    "theme": "cyborg",
+}
