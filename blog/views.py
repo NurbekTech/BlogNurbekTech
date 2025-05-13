ef39fa4e178eacce0e0097124af587a4cec0e06e
nurbekomar <omarnurbek13@gmail.com>
2025-05-13 09:27:21 +0500

    custom 404

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
 
 def home(request):
     return HttpResponse("Hello, Django!")
+
+
+def custom_404(request, exception):
+    return HttpResponseNotFound("Page Not Found")
--- core/urls.py
+++ core/urls.py
@@ -17,5 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from blog import views
 
 urlpatterns = [path("admin/", admin.site.urls), path("", include("blog.urls"))]
+
+handler404 = views.custom_404
